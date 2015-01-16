57efec5313a1ef4178f591f82dd9cac7dc3c3997
Marcos Sánchez-Dehesa <san.dehesa@gmail.com>
2015-01-17 00:03:35 +0100

    Step 2: Add Relayr Framework and header

--- complete-apps/thermometer/thermometer/ViewController.m
+++ complete-apps/thermometer/thermometer/ViewController.m
@@ -1,27 +1,16 @@
-//
-//  ViewController.m
-//  thermometer
-//
-//  Created by Marcos Sanchez-Dehesa on 16/01/2015.
-//  Copyright (c) 2015 Relayr. All rights reserved.
-//
-
-#import "ViewController.h"
+#import "ViewController.h"      // Header
+#import <Relayr/Relayr.h>       // Relayr.framework
 
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+#pragma mark - Public API
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
 }
 
 @end
