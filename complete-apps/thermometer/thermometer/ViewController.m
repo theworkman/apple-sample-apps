a47dd54e27d14cc53504acc434fb0c8d1951dc89
Marcos Sánchez-Dehesa <san.dehesa@gmail.com>
2015-01-17 00:17:20 +0100

    Step 3: Retrieve Relayr App and sign in a user

--- complete-apps/thermometer/thermometer/ViewController.m
+++ complete-apps/thermometer/thermometer/ViewController.m
@@ -1,6 +1,10 @@
 #import "ViewController.h"      // Header
 #import <Relayr/Relayr.h>       // Relayr.framework
 
+#define RelayrAppID         @"72b0e324-74bf-4e07-82a5-da53e0133a1e"
+#define RelayrAppSecret     @"_ifE7jQHgZ9DKi5-TiLBXQOAT6ibp6Zf"
+#define RelayrRedirectURI   @"https://relayr.io"
+
 @interface ViewController ()
 @end
 
@@ -11,6 +15,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [RelayrApp appWithID:RelayrAppID OAuthClientSecret:RelayrAppSecret redirectURI:RelayrRedirectURI completion:^(NSError* error, RelayrApp* app) {
+        if (error) { return; }
+        
+        [app signInUser:^(NSError* error, RelayrUser* user) {
+            if (error) { return; }
+            
+            NSLog(@"User correctly signed!");
+        }];
+    }];
 }
 
 @end
